2bd87267b55cc13ca88fd5642e19f5ff46a3dba8
minji05 <smj95133@likelion.org>
2023-08-11 08:11:13 +0900

    [Add] 홈페이지 ui & 기능 구현(1)

--- app/src/main/java/com/example/self/HomeActivity.kt
+++ app/src/main/java/com/example/self/HomeActivity.kt
@@ -2,10 +2,23 @@ package com.example.self
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val intent = intent.getStringExtra("dataFromFirstActivity")
+        val idText = findViewById<TextView>(R.id.idView)
+        idText.text ="아이디 : ${intent}"
+
+        val btn = findViewById<Button>(R.id.finishBtn)
+        btn.setOnClickListener {
+            finish()
+        }
+
     }
 }
