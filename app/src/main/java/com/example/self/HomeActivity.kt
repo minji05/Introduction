28385c39f6f6e77260df436e780ca700a353b648
minji05 <smj95133@likelion.org>
2023-08-11 08:45:35 +0900

    [Add] 주석 처리

--- app/src/main/java/com/example/self/HomeActivity.kt
+++ app/src/main/java/com/example/self/HomeActivity.kt
@@ -11,10 +11,13 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        //전달받은 데이터를 출력
         val intent = intent.getStringExtra("dataFromFirstActivity")
         val idText = findViewById<TextView>(R.id.idView)
         idText.text ="아이디 : ${intent}"
 
+
+        //종료 버튼 누를 시
         val btn = findViewById<Button>(R.id.finishBtn)
         btn.setOnClickListener {
             finish()
--- app/src/main/java/com/example/self/SignInActivity.kt
+++ app/src/main/java/com/example/self/SignInActivity.kt
@@ -13,24 +13,32 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-
+        //로그인 버튼 누를 시
         val loginbtn = findViewById<Button>(R.id.loginBtn)
         loginbtn.setOnClickListener {
+
             val idtext = findViewById<EditText>(R.id.idEditText)
             val pintext = findViewById<EditText>(R.id.pinidEditText)
+
             val idtextString = idtext.text.toString()
             val pintextString = pintext.text.toString()
+
+            //아이디/비밀번호 중 하나라도 비어 있다면 토스트 메세지 출력
             if (idtextString.isEmpty() || pintextString.isEmpty()) {
                 Toast.makeText(this, "아이디/비밀번호를 확인해주세요.", Toast.LENGTH_LONG).show()
 
-            } else{
+            } else {
+                //HomeActivity로 데이터 전달
                 val intent = Intent(this, HomeActivity::class.java)
                 intent.putExtra("dataFromFirstActivity", idtextString)
                 startActivity(intent)
+                //성공 시 토스트 메세지 띄우기
                 Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
             }
 
         }
+        
+        //회원가입 버튼 누를 시
         val signBtn = findViewById<Button>(R.id.signBtn)
         signBtn.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
