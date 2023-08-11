0409b04326cba9702301916e48fa9f5cf43ab249
minji05 <smj95133@likelion.org>
2023-08-11 09:10:00 +0900

    [Add] 랜덤 이미지 출력

--- app/src/main/java/com/example/self/HomeActivity.kt
+++ app/src/main/java/com/example/self/HomeActivity.kt
@@ -1,15 +1,27 @@
 package com.example.self
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Random
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val photos =
+            intArrayOf(R.drawable.love, R.drawable.amazed, R.drawable.anger, R.drawable.crying, R.drawable.smile)
+       //랜덤 이미지 출력
+        val image = findViewById<View>(R.id.imageView) as ImageView
+        val random = Random()
+        val i: Int = random.nextInt(photos.size)
+        image.setImageResource(photos[i])
 
         //전달받은 데이터를 출력
         val intent = intent.getStringExtra("dataFromFirstActivity")
